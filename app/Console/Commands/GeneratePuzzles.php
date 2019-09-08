b472bf2f74ab2414f495edbf83e22d552cfcf75e
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2019-09-08 15:52:43 -0400

    Add GeneratePuzzles command

---
+++ app/Console/Commands/GeneratePuzzles.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\LetterCombination;
+use App\Puzzle;
+use App\Support\PuzzleGenerator;
+
+use Date;
+use Illuminate\Console\Command;
+
+class GeneratePuzzles extends Command
+{
+    protected $signature = 'generate:puzzles {amount}';
+
+    protected $description = 'Generate the given number of puzzles.';
+
+    public function handle()
+    {
+        $start = microtime(true);
+
+        $this->comment('Generating puzzles...');
+
+        $generated = 0;
+        $amount = ($this->argument('amount') - ($this->argument('amount') % 7)) / 7;
+
+        LetterCombination::unprocessed()->inRandomOrder()->take($amount)
+            ->each(function (LetterCombination $LetterCombination) {
+                (new PuzzleGenerator($LetterCombination))->generate();
+                $generated += 7;
+            });
+
+        $end = microtime(true);
+
+        $duration = Date::createFromTimestamp($start)
+            ->shortAbsoluteDiffForHumans(Date::createFromTimestamp($end), 2);
+
+        $this->info('Generated ' . number_format($generated) . ' puzzles in ' . $duration);
+    }
+}
