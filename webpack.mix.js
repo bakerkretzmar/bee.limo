2c3c8364b4f6d1474bbf61f033b04050ae084906
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2019-09-11 20:17:50 -0400

    Disable mix notifications

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,8 @@
 const mix = require('laravel-mix')
 const path = require('path')
 
+mix.disableNotifications()
+
 mix
     .js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
