0b6dba4c63e316291bcb340d6f98be485f4a9557
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2019-10-03 20:29:54 -0400

    Turn on perpetual puzzle generation

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,10 +14,10 @@ class Kernel extends ConsoleKernel
 
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->job(new AnalyzePuzzles(100))
-        //     ->when(Puzzle::unanalyzed()->exists())
-        //     ->everyFiveMinutes()
-        //     ->withoutOverlapping();
+        $schedule->job(new AnalyzePuzzles(100))
+            ->when(Puzzle::unanalyzed()->exists())
+            ->everyFiveMinutes()
+            ->withoutOverlapping();
 
         $schedule->command('horizon:snapshot')->everyFiveMinutes();
 
