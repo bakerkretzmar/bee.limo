e136ba59ebf476b8ad009886c7070d7b20f02080
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2019-10-08 20:26:11 -0400

    Wip

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,11 +8,4 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication, DatabaseMigrations;
-
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        dump(config('database'));
-    }
 }
