3f69592238a866bad501e41c9c561706f0bf26fc
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2019-10-27 15:35:58 -0400

    Update About page

--- resources/js/Pages/About.svelte
+++ resources/js/Pages/About.svelte
@@ -25,7 +25,7 @@ p a:hover, p a:focus {
 
         <p><em>bee.limo</em> is a word game based on <a href="https://www.nytimes.com/puzzles/spelling-bee" target="_blank">Spelling Bee</a> from the <em>New York Times</em> crossword—and when I say “based on,” I mean it’s an exact copy of that game. It was a learning exercise, please don’t sue me.</p>
 
-        <p>The dictionary used to solve puzzles and generate wordlists is the <a href="https://www.wordgamedictionary.com/twl06" target="_blank">‘TWL06’</a> Scrabble players’ dictionary, which contains about 178,000 English words. The list of 7-letter combinations used to build new puzzles was generated with the delightful MathsIsFun.com <a href="https://www.mathsisfun.com/combinatorics/combinations-permutations-calculator.html" target="_blank">Combinations and Permutations Calculator</a>.</p>
+        <p>The words used in this game are those in the <a href="https://www.scrabbleplayers.org/w/NWL2018" target="_blank">NASPA Word List</a> 2018 Edition, published by the North American Scrabble Players’ Association. The list of 7-letter combinations used to build new puzzles was generated with the delightful MathsIsFun.com <a href="https://www.mathsisfun.com/combinatorics/combinations-permutations-calculator.html" target="_blank">Combinations and Permutations Calculator</a>.</p>
 
         <p><em>bee.limo</em> is built with <a href="https://laravel.com" target="_blank">Laravel</a>, <a href="https://svelte.dev" target="_blank">Svelte</a>, and <a href="https://tailwindcss.com" target="_blank">Tailwind</a>. It’s open source—you can browse and download the source code for the project <a href="https://github.com/bakerkretzmar/spelling-bee" target="_blank">here</a>.</p>
 
