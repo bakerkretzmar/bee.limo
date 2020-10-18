c2cf1e3431dfc346bb18f36d8a2fca0695eb9430
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2020-10-17 22:17:22 -0400

    Favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link rel="shortcut icon" type="image/png" href="/icon.png"/>
         <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
         <link href="https://use.typekit.net/sto6skh.css" rel="stylesheet">
         @routes
