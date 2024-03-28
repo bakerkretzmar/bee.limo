e021deb0fa57fc060df3abd831ec9469bdd89a54
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2024-03-27 20:07:28 -0400

    Skeleton wip

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -7,13 +7,15 @@ use Illuminate\Support\Str;
 
 class UserFactory extends Factory
 {
+    protected static ?string $password;
+
     public function definition(): array
     {
         return [
-            'name' => $this->faker->name,
-            'email' => $this->faker->unique()->safeEmail,
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail,
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
     }
